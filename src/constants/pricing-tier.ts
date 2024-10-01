fb29ea22b64f43772cb8a928826bcb8e835970ad
Haoran Wang <legendwhr@gmail.com>
2024-10-01 22:12:33 +0800

    add SMP

--- src/constants/pricing-tier.ts
+++ src/constants/pricing-tier.ts
@@ -1,6 +1,6 @@
 export interface Tier {
   name: string;
-  id: 'starter' | 'pro' | 'advanced';
+  id: 'mobile' | 'super' | 'premium';
   icon: string;
   description: string;
   features: string[];
@@ -10,26 +10,26 @@ export interface Tier {
 
 export const PricingTier: Tier[] = [
   {
-    name: 'Starter',
-    id: 'starter',
+    name: 'Mobile',
+    id: 'mobile',
     icon: '/assets/icons/price-tiers/free-icon.svg',
     description: 'Ideal for individuals who want to get started with simple design tasks.',
     features: ['1 workspace', 'Limited collaboration', 'Export to PNG and SVG'],
     featured: false,
-    priceId: { month: 'pri_01hsxyh9txq4rzbrhbyngkhy46', year: 'pri_01hsxyh9txq4rzbrhbyngkhy46' },
+    priceId: { month: 'pri_01j945hntx796kb2m4nzm6qcsj', year: 'pri_01j945jd3tvw25wr8rz74zvh95' },
   },
   {
-    name: 'Pro',
-    id: 'pro',
+    name: 'Super',
+    id: 'super',
     icon: '/assets/icons/price-tiers/basic-icon.svg',
     description: 'Enhanced design tools for scaling teams who need more flexibility.',
     features: ['Integrations', 'Unlimited workspaces', 'Advanced editing tools', 'Everything in Starter'],
     featured: true,
-    priceId: { month: 'pri_01hsxycme6m95sejkz7sbz5e9g', year: 'pri_01hsxyeb2bmrg618bzwcwvdd6q' },
+    priceId: { month: 'pri_01j945kg2f4w8et55vt4f5vk7c', year: 'pri_01j945kzdbkv71rqww053z3gcw' },
   },
   {
-    name: 'Advanced',
-    id: 'advanced',
+    name: 'Premium',
+    id: 'premium',
     icon: '/assets/icons/price-tiers/pro-icon.svg',
     description: 'Powerful tools designed for extensive collaboration and customization.',
     features: [
@@ -40,6 +40,6 @@ export const PricingTier: Tier[] = [
       'Everything in Pro',
     ],
     featured: false,
-    priceId: { month: 'pri_01hsxyff091kyc9rjzx7zm6yqh', year: 'pri_01hsxyfysbzf90tkh2wqbfxwa5' },
+    priceId: { month: 'pri_01j945mx518vzm9t5rc2sedjw5', year: 'pri_01j945nb66s6qen2jxafbbgsbg' },
   },
 ];
